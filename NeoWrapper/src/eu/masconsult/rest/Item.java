e3b6926e4ef4e0f832b683ba544bca1e9279bc31
Daniel Petrov <dani@masconsult.eu>
2011-08-18 15:00:58 +0300

    fixed web project

--- NeoWrapper/src/eu/masconsult/rest/Item.java
+++ NeoWrapper/src/eu/masconsult/rest/Item.java
@@ -1,31 +1,46 @@
 package eu.masconsult.rest;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
- * User: Mas Consult
- * Date: 15-08-2011
+ * User: Mas Consult Date: 15-08-2011
  */
+@XmlRootElement
 public class Item {
-    private String name;
+  private String name;
 
-    public Item(String name) {
-        this.name = name;
-    }
+  public Item() {
+  }
 
-    public String getName() {
-        return name;
-    }
+  public Item(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) return true;
-        if (other == null || getClass() != other.getClass()) return false;
-        Item item = (Item) other;
-        if (name != null ? !name.equals(item.name) : item.name != null) return false;
-        return true;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
     }
+    if ((other == null) || (getClass() != other.getClass())) {
+      return false;
+    }
+    Item item = (Item) other;
+    if (name != null ? !name.equals(item.name) : item.name != null) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
 }
--- NeoWrapperWeb/src/eu/masconsult/ServiceServlet.java
+++ NeoWrapperWeb/src/eu/masconsult/ServiceServlet.java
@@ -11,59 +11,69 @@ import javax.ws.rs.core.MediaType;
 import eu.masconsult.rest.Item;
 import eu.masconsult.rest.ItemService;
 
-@Path("/item")
+@Path("item")
 @Produces(MediaType.APPLICATION_JSON)
 public class ServiceServlet implements ItemService {
 
-	public void add(Item item) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public void add(Item item) {
+    // TODO Auto-generated method stub
 
-	@GET
-	@Path("{name}")
-	public Item find(@PathParam("name") String name) {
-		return new Item(name);
-	}
+  }
 
-	public boolean isAvailable(Item item) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+  @Override
+  @GET
+  @Path("{name}")
+  public Item find(@PathParam("name") String name) {
+    return new Item(name);
+  }
 
-	public void remove(Item item) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public boolean isAvailable(Item item) {
+    // TODO Auto-generated method stub
+    return false;
+  }
 
-	public Collection<Item> findAdjacent(Item item) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public void remove(Item item) {
+    // TODO Auto-generated method stub
 
-	public Item createItem(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  }
 
-	public void connect(Item parent, Item child) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public Collection<Item> findAdjacent(Item item) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	public Collection<Item> findConnected(Item item) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Item createItem(String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	public Collection<Item> largestItemCollection() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public void connect(Item parent, Item child) {
+    // TODO Auto-generated method stub
 
-	public void finish() {
-		// TODO Auto-generated method stub
-		
-	}
+  }
+
+  @Override
+  public Collection<Item> findConnected(Item item) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Collection<Item> largestItemCollection() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void finish() {
+    // TODO Auto-generated method stub
+
+  }
 
 }
